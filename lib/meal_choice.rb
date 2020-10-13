d9dbb34fbc5d7dd56922b9c46c52fddad27a6363
Alexa Botello <abotello20@yahoo.com>
2020-10-13 04:50:10 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,3 +1,8 @@
 def meal_choice(veg1, veg2, protein = 'tofu')
   puts "What a nutiritious meal!"
+  puts ""
+end
+
+def meal_choice(veg1, veg2, protein = 'meat')
+  puts "What a nutiritious meal!"
 end
